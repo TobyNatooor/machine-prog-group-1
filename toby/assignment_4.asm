25ef6bcf7d00a5ee12936eda683264dfba54b5e2
Tobias <s235086@student.dtu.dk>
2024-03-18 17:22:41 +0100

    updated comments

--- toby/assignment_4.asm
+++ toby/assignment_4.asm
@@ -4,29 +4,29 @@
 
 .ORIG x3000
         
-        AND R0, R0, x0
+        ; Tests for the function
+        AND R0, R0, #0
         ADD R0, R0, #1
         JSR resultS
-        AND R0, R0, x0
+        AND R0, R0, #0
         ADD R0, R0, #0
         JSR resultS
-        AND R0, R0, x0
+        AND R0, R0, #0
         ADD R0, R0, #-1
         JSR resultS
-
         HALT
         
-resultS AND R1, R1, x0
-        ADD R1, R1, #1
-        AND R1, R1, R0
-        BRz NOT_P
-        LEA R0, IS_P_STR
-        PUTS
-        RET
-NOT_P   LEA R0, NO_P_STR
-        PUTS
-        RET
+resultS AND R1, R1, #0      ; Set R1 to 0
+        ADD R1, R1, #1      ; Add R1 by 1
+        AND R1, R1, R0      ; And R1 and R0 to see if R0 contains a 1 or 0 as the bit most to the right
+        BRz NOT_P           ; If the result is 0, go to NOT_P
+        LEA R0, IS_P_STR    ; Load the address of the string IS_P_STR into R0
+        PUTS                ; Print IS_P_STR to the console
+        RET                 ; Return from the subrutine
+NOT_P   LEA R0, NO_P_STR    ; Load the address of the string IS_P_STR into R0
+        PUTS                ; Print NO_P_STR to the console
+        RET                 ; Return from the subrutine
         
-IS_P_STR .STRINGZ "The number is prime\n"
-NO_P_STR .STRINGZ "The number is not prime\n"
+IS_P_STR .STRINGZ "The number is prime\n"       ; The string that gets printed if R0 is not 0, and therefore is prime
+NO_P_STR .STRINGZ "The number is not prime\n"   ; The string that gets printed if R0 is 0, and therefore is not prime
 .END
